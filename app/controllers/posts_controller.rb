c9f5d79c26f2d35c8c7342faa17cfe05eb2dadd9
Ryan Pethel <arpethel@gmail.com>
2019-08-02 07:30:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
     redirect_to post_path(@post)
   end
